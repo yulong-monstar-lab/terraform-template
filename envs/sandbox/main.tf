8b4a5cb332d09958261d150e7003a32c40095a19
wyljpn <wyljpn@gmail.com>
2023-08-25 15:36:48 +0900

    feat: added versions.tf and moved things realted to versions to it

--- envs/sandbox/main.tf
+++ envs/sandbox/main.tf
@@ -1,20 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.69.0"
-    }
-  }
-}
 
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
 
 data "azurerm_client_config" "current" {}
 
