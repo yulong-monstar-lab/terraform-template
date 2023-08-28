d9188da2c259837930281d787fc5bc8e49d08c10
yulong-monstar-lab <yulong.wang@monstar-lab.com>
2023-08-28 11:08:09 +0900

    feat: removed some files

--- modules/azure/networking/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "virtual_network_id" {
-  value = azurerm_virtual_network.virtual_network.id
-}
-
-
-output "private_subnet_id" {
-  value = azurerm_subnet.private_subnet.id
-}
--- modules/azure/webapp/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "web_app_id" {
-  value = azurerm_linux_web_app.webapp.id
-}
-
-
