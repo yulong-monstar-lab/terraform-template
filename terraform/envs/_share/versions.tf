2716031ff143a425e0f882fd996eeefd775b2c28
ysano <ysano@ysnet.org>
2023-08-29 14:58:20 +0900

    update _share

--- terraform/envs/_share/versions.tf
+++ terraform/envs/_share/versions.tf
@@ -2,22 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.69.0"
+      version = ">= 3.69.0"
     }
-  }
-  backend "azurerm" {
-    resource_group_name   = "komatsu-dx-app"
-    storage_account_name  = "komatsustorageaccount"
-    container_name        = "mycontainer"
-    key                   = "terraform.tfstate"
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.8.0"
+    }
   }
 }
 
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
