b1abde505c8a96285a57858c4c3fe1f80d034a57
wyljpn <wyljpn@gmail.com>
2024-11-15 12:25:43 +0900

    change resource group and storage

--- terraform/envs/sandbox/backend.tf
+++ terraform/envs/sandbox/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "komatsu-dx-app"
-    storage_account_name  = "komatsustorageaccount"
+    resource_group_name   = "cicd-test"
+    storage_account_name  = "cicdtestyulong"
     container_name        = "mycontainer"
     key                   = "terraform.tfstate"
   }
